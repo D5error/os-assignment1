4cca4863126cdb672a2fa2e4eb1b70700d6cdd64
d5error <d5error@163.com>
2025-04-21 14:39:18 +0800

    update

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -5,13 +5,12 @@
 docker rm -f my-container
 
 # 编译
-docker build --network host -t my-ubuntu24 .
+docker build --network host -t os-assignment-1 .
 
 # 运行容器
 docker run \
     -v "$(pwd)/../client_server:/home/ubuntu/os-assignment-1/client_server" \
     -v "$(pwd)/../xv6-labs-2024:/home/ubuntu/os-assignment-1/xv6-labs-2024" \
     -w "/home/ubuntu/os-assignment-1" \
-    --name my-container \
-    -it my-ubuntu24 bash
+    -it os-assignment-1 bash
 
