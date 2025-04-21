0044e55f87aa619ad12ecc6fa6b902408bb5c8ba
d5error <d5error@163.com>
2025-04-21 15:24:05 +0800

    update

--- xv6-labs-2024/user/find.d
+++
@@ -1,2 +0,0 @@
-user/find.o: user/find.c /usr/riscv64-linux-gnu/include/stdc-predef.h \
- kernel/types.h kernel/fs.h user/user.h kernel/fcntl.h kernel/stat.h
--- xv6-labs-2024/user/pingpong.d
+++
@@ -1,3 +0,0 @@
-user/pingpong.o: user/pingpong.c \
- /usr/riscv64-linux-gnu/include/stdc-predef.h kernel/types.h user/user.h \
- /usr/lib/gcc-cross/riscv64-linux-gnu/13/include/stddef.h
